b0bd2dbaa7d4ac50adab89c80eac3841587b9a32
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2021-07-20 09:20:06 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,83 +1,64 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_LINHAS = 3;
-const int MAX_COLUNAS = 4;
+const int MAX_MOVIES = 5;
+const int MAX_OPINIONS = 6;
+const int OPINIONS = 5;
+const int MAX_CHARS = 81;
 
-/* 
-  leNotas (notas, linhas, colunas)
-  notas: matriz de notas com o tamanho máximo MAX_LINHAS por MAX_COLUNAS.
-  linhas: quantidade de linhas da matriz notas.
-  colunas: quantidade de colunas da matriz notas.
-  Realiza a leitura de notas com valores de 0 até 10.
-*/
-void leNotas (float notas[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas) {
-  for (int linha = 0; linha < linhas; linha++) {
-    for (int coluna = 0; coluna < colunas; coluna++) {
-      do {
-        cin >> notas[linha][coluna];
-      } while (notas[linha][coluna] < 0 || notas[linha][coluna] > 10);
-    }
-  }
+struct Movie {
+  int idMovie;
+  char title[MAX_CHARS];
+  int year;
+};
+
+struct Opinions {
+  int idMovie;
+  int stars[MAX_OPINIONS];
+};
+
+void printMovie (Movie movie) {
+  cout << movie.year << ": " 
+       << movie.title;
 }
 
-/* 
-  calculeMedias (notas, linhas, colunas, medias)
-  Calcula as médias das notas por cada linha da matriz.
-*/
-void calculeMedias (float notas[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas, float medias[MAX_LINHAS]) {
-  float soma;
+float calculateStars (Opinions opinions[], int idMovie) {
+  float meanStars = 0;
+
   /*
-  PARA cada linha da matriz EXECUTE
-    a variável soma inicia com valor 0 (zero)
-    PARA cada coluna da matriz EXECUTE
-      soma recebe a soma anterior mais a j-ésima nota da linha
+    PARA cada posição do vetor opinions EXECUTE
+      SE opinions[i].idMovie == idMovie ENTÃO
+        PARA cada posição do vetor opinions[i].star EXECUTE
+          some cada uma das "estrelas" de opinions[i].star[s]
+        FIM-PARA
+      FIM-SE
     FIM-PARA
-    a média do i-ésimo aluno recebe soma dividido pela quantidade de notas
-  FIM-PARA
   */
-}
 
-/* 
-  mostreMedias (medias, linhas)
-  Mostra as médias calculadas para cada linha da matriz.
-*/
-void mostreMedias (float medias[MAX_LINHAS], int linhas) {
-  cout << "Medias: ";
-  for (int i = 0; i < linhas; i++) {
-    cout << medias[i] << " ";
-  }
-  cout << endl;
-}
-
-/* 
-  maiorMedia (medias, linhas)
-  Mostre a maior média e qual foi o aluno que obteve essa média.
-*/
-void maiorMedia (float medias[MAX_LINHAS], int linhas) {
-  int   aluno = 0;
-  float maior = medias[0];
-
-  for (int i = 1; i < linhas; i++) {
-    if (medias[i] > maior) {
-      maior = medias[i];
-      aluno = i;
-    }
-  }
-  cout << "A maior media eh " << maior << " do aluno " << (aluno+1) << endl;
+  return meanStars / MAX_OPINIONS;
 }
 
 int main() {
-  float notas[MAX_LINHAS][MAX_COLUNAS];
-  float medias[MAX_LINHAS];
+  Movie movies[MAX_MOVIES] = {
+    { 101, "Star Wars - Nova Esperanca: Episodio 4", 1977 },
+    { 102, "Star Wars - O Imperio Contra-Ataca: Episodio 5", 1980 },
+    { 103, "Star Wars - O Retorno do Jedi: Episodio 6", 1983 }, 
+    { 201, "Um sonho de liberdade", 1994 },
+    { 202, "Pulp Fiction", 1994 }
+  };
 
-  leNotas (notas, MAX_LINHAS, MAX_COLUNAS);
+  Opinions opinions[OPINIONS] = {
+    { 101, { 3, 4, 5, 5, 5, 5 } },
+    { 201, { 4, 4, 5, 4, 5, 5 } },
+    { 102, { 4, 4, 3, 5, 5, 3 } },
+    { 202, { 4, 4, 3, 5, 5, 3 } },
+    { 103, { 4, 3, 4, 5, 5, 3 } }
+  };
 
-  calculeMedias (notas, MAX_LINHAS, MAX_COLUNAS, medias);
-
-  mostreMedias (medias, MAX_LINHAS);
-
-  maiorMedia (medias, MAX_LINHAS);
+  for (int i = 0; i < MAX_MOVIES; i++) {
+    printMovie (movies[i]);
+    cout << " had " << calculateStars (opinions, movies[i].idMovie) << " stars\n"; 
+  }
 
   return 0;
 }
